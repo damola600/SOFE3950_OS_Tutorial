d3c8c4eaae7e06f83e7b35f37dd9cdb1ad02e73c
Justin K -Linux <justinjoseph0007@gmail.com>
2018-03-05 11:13:57 -0500

    Simplified question 02

--- Tutorial 6/Question1.c
+++ Tutorial 6/Question1.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h> /* For sleep() */
 
@@ -18,21 +17,21 @@ void read_file(const char* filename)
 
 int main()
 {
-    pid_t pid;
+    int pid;
     int status        = 0;
     const char* file1 = "child1.txt";
-    const char* file2 = "child2.txt";
-    FILE* child1      = fopen(file1, "w");
-    FILE* child2      = fopen(file2, "w");
-    if (child1 == NULL || child2 == NULL) {
-        printf("File for writing could not be opened!!.. exiting...");
-        exit(EXIT_FAILURE);
-    }
-    fprintf(child1, "Child 1\n");
-    fprintf(child2, "Child 2\n");
-    fclose(child2);
-    fclose(child1);
-    if (fork() == 0) /* This child1 process */
+     const char* file2 = "child2.txt";
+     FILE* child1      = fopen(file1, "w");
+     FILE* child2      = fopen(file2, "w");
+     if (child1 == NULL || child2 == NULL) {
+	 printf("File for writing could not be opened!!.. exiting...");
+	 exit(EXIT_FAILURE);
+     }
+     fprintf(child1, "Child 1\n");
+     fprintf(child2, "Child 2\n");
+     fclose(child2);
+     fclose(child1);
+     if (fork() == 0) /* This child1 process */
         read_file(file1);
     else {
         if (fork() == 0) /* This is child2 */
