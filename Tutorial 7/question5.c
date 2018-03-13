f7eee3848165b4d5abd78bebeef2d5a1dd068999
Justin K -Linux <justinjoseph0007@gmail.com>
2018-03-12 22:54:21 -0400

    Added config for question 5

---
+++ Tutorial 7/question5.c
@@ -0,0 +1,14 @@
+#include <signal.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main()
+{
+
+    return 0;
+}
