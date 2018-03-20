444b4804f909c6655fd9c2f6a90f7121877bd272
Justin K -Linux <justinjoseph0007@gmail.com>
2018-03-20 14:51:05 -0400

    Added the makefile and setup before implementation

---
+++ Tutorial 8/Question2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[])
+{
+    printf("hello");
+    return 0;
+}
